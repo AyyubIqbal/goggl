7e9ac0d9ce23ed797946a05b09611e53a7882cb8
Ayyub Iqbal <abuayubiqbal@gmail.com>
2021-10-21 11:37:48 +0600

    context api and google serach api added

--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext()
 const baseURL = 'https://google-search3.p.rapidapi.com/api/v1';
@@ -12,6 +12,25 @@ export const ResultContextProvider = ({ children }) => {
     const getResult = async (type) => {
         setIsLoading(true)
 
-        const response = await fetch(`${baseURL}${type}`)
+        const response = await fetch(`${baseURL}${type}`, {
+            method: 'GET',
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': '273e4ba326msh79ad4365319dc62p143635jsnaedaed0ca4c2'
+            }
+        });
+
+        const data = await response.json()
+
+        setResults(data)
     }
+
+    return (
+        <ResultContext.Provider value={{ getResult, results, searchTerm, setSearchTerm, isLoading }}>
+            {children}
+        </ResultContext.Provider>
+    )
 }
+
+export const useResultContext = () => useContext(ResultContext)
